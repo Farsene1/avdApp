b5e2a8da1b2a6448fd6d83a89c1c2d89e683812a
Florentin Arsene <F.Arsene@student.tudelft.nl>
2020-02-03 22:45:28 +0200

    added database connection working

--- ExpenseItHome.xaml.cs
+++ ExpenseItHome.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+using Finisar.SQLite;
 namespace WpfApp3
 {
     /// <summary>
@@ -30,6 +31,37 @@ namespace WpfApp3
             // View Expense Report
             ExpenseReportPage expenseReportPage = new ExpenseReportPage(this.peopleListBox.SelectedItem);
             this.NavigationService.Navigate(expenseReportPage);
+
+
+            SQLiteConnection sqlite_conn;
+            SQLiteCommand sqlite_cmd;
+            SQLiteDataReader sqlite_datareader;
+
+            // create a new database connection:
+            sqlite_conn = new SQLiteConnection("Data Source=database.db;Version=3;New=False;Compress=True;");
+
+            // open the connection:
+            sqlite_conn.Open();
+
+            // create a new SQL command:
+            sqlite_cmd = sqlite_conn.CreateCommand();
+
+            // But how do we read something out of our table ?
+            // First lets build a SQL-Query again:
+            sqlite_cmd.CommandText = "SELECT * FROM test";
+
+            // Now the SQLiteCommand object can give us a DataReader-Object:
+            sqlite_datareader = sqlite_cmd.ExecuteReader();
+
+            // The SQLiteDataReader allows us to run through the result lines:
+            while (sqlite_datareader.Read()) // Read() returns true if there is still a result line to read
+            {
+                // Print out the content of the text field:
+                System.Console.WriteLine(sqlite_datareader["text"]);
+            }
+
+            // We are ready, now lets cleanup and close our connection:
+            sqlite_conn.Close();
         }
     }
 }
